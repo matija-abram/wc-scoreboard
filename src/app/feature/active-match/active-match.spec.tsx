19d47b782992e0646325b994be72cdee89b92437
Matija Abram <1071997+matija-abram@users.noreply.github.com>
2023-08-07 19:04:57 +0200

    test: fix component test

--- src/app/feature/active-match/active-match.spec.tsx
+++ src/app/feature/active-match/active-match.spec.tsx
@@ -1,10 +1,13 @@
 import { render } from '@testing-library/react';
-
 import ActiveMatch from './active-match';
+
+import { Match } from '../../lib/game/match';
+import { Team } from '../../lib/team/team';
 
 describe('ActiveMatch', () => {
     it('should render successfully', () => {
-        const { baseElement } = render(<ActiveMatch />);
+        const match = new Match(new Team('abc'), new Team('def'));
+        const { baseElement } = render(<ActiveMatch match={match} />);
         expect(baseElement).toBeTruthy();
     });
 });
